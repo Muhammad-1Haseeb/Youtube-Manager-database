2c80584376fabec7223bae633eeb0d1ebf78a66b
Muhammad-1Haseeb <your-email@example.com>
2026-08-09 00:13:29 +0500

    completed the youtube manager app with database sqlite3

--- youtube_manager_db.py
+++ youtube_manager_db.py
@@ -12,9 +12,54 @@ cursor.execute('''
     )
 ''')
 
+def list_videos():
+    cursor.execute("SELECT * FROM videos")
+    for row in cursor.fetchall():
+          print(row)
+
+def add_video(name, time):
+    cursor.execute("INSERT INTO videos (name, time) VALUES (?, ?)", (name, time))
+    conn.commit()
+
+def update_video(video_id, new_name, new_time):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (new_name, new_time, video_id))
+    conn.commit()
+
+def delete_video(video_id):
+    cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
+    conn.commit()
 def main():
     while True:
         print("\n Youtube Video Manager app with DB")
+        print("1. List all Youtube Videos")
+        print("2. Add a new Youtube Video")
+        print("3. Update a Youtube Video")
+        print("4. Delete a Youtube Video")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
 
+        if choice == '1':
+            list_videos()
+
+        elif choice == '2':
+            name = input("Enter video name: ")
+            time = input("Enter video time: ")
+            add_video(name, time)
+
+        elif choice == '3':
+                    video_id = input("Enter video ID you want to update: ")
+                    name = input("Enter video name: ")
+                    time = input("Enter video time: ")
+                    update_video(video_id,name, time)
+        elif choice == '4':
+                    video_id = input("Enter video ID you want to delete: ")
+                    delete_video(video_id)
+        elif choice == '5':
+                    break
+
+        else:
+              print("Invalid choice. Please try again.")
+
+    conn.close()
 if __name__ == "__main__":
     main()
